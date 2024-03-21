2fa74f892b877c25d7a2cc88e2fcb4d6aa984d64
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-03-21 18:10:50 +0000

    Simple heap allocated array

--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,21 @@
 const std = @import("std");
 
-pub fn main() !void {}
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var my_array = try allocator.alloc(isize, 1024); // create a 1kb array of isize
+    defer allocator.free(my_array);
+
+    var counter: u16 = 0;
+
+    for (my_array, 0..) |_, i| {
+        counter += 1;
+        my_array[i] = counter;
+    }
+
+    std.debug.print("my_array data type -> {}\n", .{@TypeOf(my_array)});
+    std.debug.print("my_array[0] data type -> {}\n", .{@TypeOf(my_array[0])});
+    std.debug.print("my_array -> {any}\n", .{my_array});
+}
