1b8ecda85026a61c359b65b551cb1e5f43db263c
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-04-08 17:35:26 +0100

    Recursion in prgress

--- src/algorithms/recursion.zig
+++ src/algorithms/recursion.zig
@@ -22,10 +22,10 @@ const Point = struct {
     y: usize,
 };
 
-pub fn mazeSolver(maze: []const u8, wall: []const u8, start: Point, end: Point) []Point {}
+pub fn mazeSolver(maze: [][]u8, wall: []const u8, start: Point, end: Point) []Point {}
 
-fn mazeWalker(maze: []const u8, wall: []const u8, current: Point, end: Point, seen: [][]bool) bool {
-    // base cases
+fn mazeWalker(maze: [][]u8, wall: []const u8, current: Point, end: Point, seen: [][]bool, path: []Point) bool {
+    // BASE CASE
     // outside the bounds of the array
     if (current.x < 0 or current.x >= maze[0].len or current.y < 0 or current.y >= maze.len) return false;
     // on a wall
@@ -34,4 +34,13 @@ fn mazeWalker(maze: []const u8, wall: []const u8, current: Point, end: Point, se
     if (current.x == end.x and current.y == end.y) return true;
     // have we seen this point
     if (seen[current.x][current.y]) return false;
+
+    // RECURSE CASE
+    // pre
+    // add current to []path - ArrayList? needs to push/pop
+    // recurse
+    // loop over directions (up, right, down, left)
+    // recurse over the directions
+    // post
+    // remove current from []path
 }
