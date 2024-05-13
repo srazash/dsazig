f1ec7985e1cded8e03c0e8da422cc3d30b5ac928
Ryan Shaw-Harrison <srazash@protonmail.com>
2024-05-13 17:25:16 +0100

    Added getParent(), getLeftChild() and getRightChild().

--- src/data_structures/heap.zig
+++ src/data_structures/heap.zig
@@ -19,5 +19,37 @@ pub fn Heap(comptime T: type) type {
         pub fn deinit(self: *Self) void {
             self.data.deinit();
         }
+
+        pub fn add(self: *Self, data: T) !void {
+            try self.data.append(data);
+            self.length += 1;
+        }
+
+        fn getParent(current: usize) !usize {
+            if (current == 0) return 0;
+
+            if (try std.math.mod(usize, current, 2) == 0)
+                return (current - 2) / 2;
+
+            return (current - 1) / 2;
+        }
+
+        fn getLeftChild(self: *Self, current: usize) !usize {
+            if (self.length < 2) return 0;
+
+            const index = (2 * current) + 1;
+
+            if (index < self.length) return index;
+            return error.OutOfBounds;
+        }
+
+        fn getRightChild(self: *Self, current: usize) !usize {
+            if (self.length < 2) return 0;
+
+            const index = (2 * current) + 2;
+
+            if (index < self.length) return index;
+            return error.OutOfBounds;
+        }
     };
 }
